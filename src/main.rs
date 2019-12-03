f144a3fcd020384f4aa30ebc4aaa14029e6422be
Robert Brown <ammoknight@gmail.com>
2019-12-03 09:02:49 -0700

    Add run time measurement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use std::time::Instant;
+
 mod day1;
 mod day2;
 mod day3;
 
 fn main() {
+    let start = Instant::now();
+
     day1::solve();
     day2::solve();
     day3::solve();
+
+    let duration = Instant::now().duration_since(start);
+    println!();
+    println!("Ran in {:?}", duration);
 }
