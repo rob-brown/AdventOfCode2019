17f2dd2047dc63916a67d162108469c9b103d74e
Robert Brown <ammoknight@gmail.com>
2019-12-26 21:21:40 -0700

    Add day 21 part 2

--- src/aoc/day21.rs
+++ src/aoc/day21.rs
@@ -3,7 +3,8 @@ use super::intcode::Machine;
 use std::io::stdin;
 
 pub fn solve() {
-    let mut machine = Machine::from_file("input/day21.txt");
+    let initial = Machine::from_file("input/day21.txt");
+    let mut machine = Machine::init(&initial.positions);
     let commands = [
     "NOT A J",
 	"NOT C T",
@@ -15,4 +16,27 @@ pub fn solve() {
 
     machine.run(commands);
     assert_eq(Day::new(21, Part::A), 19354890, machine.values.pop_back().unwrap());
+
+    let mut machine = Machine::init(&initial.positions);
+    let commands = [
+    "NOT A J",
+	"AND D J",
+	"NOT B T",
+	"AND D T",
+	"AND H T",
+	"OR T J",
+	"NOT C T",
+	"AND D T",
+	"AND E T",
+	"OR T J",
+	"NOT C T",
+	"AND D T",
+	"AND H T",
+	"OR T J",
+    "RUN",
+    ""
+    ].join("\n").chars().map(|x| x as i64).rev().collect::<Vec<i64>>();
+
+    machine.run(commands);
+    assert_eq(Day::new(21, Part::B), 1140664209, machine.values.pop_back().unwrap());
 }
