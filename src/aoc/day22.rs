9e1ebb7fe7cb302069307e2d4b99d27a435f620a
Robert Brown <ammoknight@gmail.com>
2019-12-27 00:36:29 -0700

    Add day 22 part 1

--- src/aoc/day22.rs
+++ src/aoc/day22.rs
@@ -1,19 +1,28 @@
+use super::assert::*;
 use regex::Regex;
 use std::fs::File;
 use std::io::prelude::*;
 
-const CARD_COUNT: isize = 10_007;
+const CARD_COUNT: i64 = 10_007;
 
-fn track_position<T>(index: isize, command: &str, number: Result<isize, T>) -> isize {
-    match (command, number) {
-        ("cut", Ok(count)) => (index - count) % CARD_COUNT,
+enum Shuffle {
+    Cut(i64),
+    Increment(i64),
+    Reverse,
+}
 
-        ("deal with increment", Ok(count)) => (index * count).rem_euclid(CARD_COUNT),
+fn track_position(mut index: i64, commands: &Vec<Shuffle>) -> i64 {
+    for c in commands.iter() {
+        match c {
+            Shuffle::Cut(count) => index = (index - count) % CARD_COUNT,
 
-        ("deal into new stack", _) => CARD_COUNT - index - 1,
+            Shuffle::Increment(count) => index = (index * count).rem_euclid(CARD_COUNT),
 
-        _ => panic!(),
+            Shuffle::Reverse => index = CARD_COUNT - index - 1,
+        }
     }
+
+    index
 }
 
 pub fn solve() {
@@ -22,14 +31,22 @@ pub fn solve() {
     file.read_to_string(&mut contents).unwrap();
     let regex = Regex::new(r"([a-z][ a-z]+[a-z]) ?(-?\d*)\n").unwrap();
 
-    let mut index = 2019;
+    let mut commands: Vec<Shuffle> = Vec::new();
 
     for capture in regex.captures_iter(&contents) {
         let command = capture[1].parse::<String>().unwrap();
         let number = capture[2].parse::<String>().unwrap();
 
-        index = track_position(index, command.as_str(), number.parse::<isize>());
+        match command.as_str() {
+            "cut" => commands.push(Shuffle::Cut(number.parse::<i64>().unwrap())),
+
+            "deal with increment" => commands.push(Shuffle::Increment(number.parse::<i64>().unwrap())),
+
+            "deal into new stack" => commands.push(Shuffle::Reverse),
+
+            _ => panic!(),
+        }
     }
 
-    assert_eq(Day::new(22, Part::A), 5755, index);
+    assert_eq(Day::new(22, Part::A), 5755, track_position(2019, &commands));
 }
